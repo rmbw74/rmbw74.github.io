73903cd72a39bef12ae2151e3b8c6622886067be
Ray Medrano <rmbw74@gmail.com>
2017-11-20 14:00:59 -0600

    got projects displaying from firebase instead of local storage

--- projects/scripts/projects-controller.js
+++ projects/scripts/projects-controller.js
@@ -1,19 +1,12 @@
 //retrieve the blog entries stored in local storage and place into object
-const retrievedProjectEntries = JSON.parse(localStorage.getItem("projectHistory"));
+debugger
+const getProjects = $.ajax({ url: "https://personalsite-9ec60.firebaseio.com/projectEntries.json" })
 
-//target the DOM element that will contain the blog articles
-const element = document.getElementById("projectArticleWrapper");
-
-//iterate over the key in the retrievedBlogEntries object
-for (let key in retrievedProjectEntries) {
-
-    //set current equal to the current key in the object
-    let current = retrievedProjectEntries[key];
-    //iterate over the values in the  array in the current key and build string to inject into the DOM
-    for (let i = 0; i < current.length; i++) {
-        //set the currentitem to the value of the current array
-        let currentitem = current[i];
-        element.innerHTML += `<article class="article under">
+getProjects.then(result => {
+    //target the DOM projectEl that will contain the blog articles
+    const projectEl = document.getElementById("projectArticleWrapper");
+    result.forEach(currentitem => {
+        projectEl.innerHTML += `<article class="article under">
                                 <section class="articleHeader">
                                     <p>${currentitem.projectName}</p>
                                 </section>
@@ -30,5 +23,19 @@ for (let key in retrievedProjectEntries) {
                                     <p class="article__text">Project Link: <a href="${currentitem.linkToView}">Click Here to View</a></p>
                                 </section>
                              </article>`
-    }
-}
+
+    })
+   /*  //iterate over the key in the retrievedBlogEntries object
+    for (let key in result) {
+
+        //set current equal to the current key in the object
+        let current = result[key];
+        //iterate over the values in the  array in the current key and build string to inject into the DOM
+        for (let i = 0; i < current.length; i++) {
+            //set the currentitem to the value of the current array
+            let currentitem = current[i];
+        }
+    } */
+
+
+})
