6924557422609e505349fdb8bcd15623618c0e7c
Ray Medrano <rmbw74@gmail.com>
2017-11-20 14:24:14 -0600

    got resume working from firebase instead of local storage

--- resume/scripts/resume-controller.js
+++ resume/scripts/resume-controller.js
@@ -1,8 +1,21 @@
 //retrieve the blog entries stored in local storage and place into object
 const retrievedJobEntries = JSON.parse(localStorage.getItem("jobHistory"));
-
-//target the DOM element that will contain the blog articles
+const getResume = $.ajax({ url: "https://personalsite-9ec60.firebaseio.com/resumeEntries.json" })
 const element = document.getElementById("jobArticleWrapper");
+
+getResume
+.then(result => {
+  result.forEach(currentitem => {
+    element.innerHTML += `<article class="article under">
+                          <section class="article__company"><br>${currentitem.company}</section>
+                          <section class="articleHeader">${currentitem.title}</section>
+                          <section class="article__duration">${currentitem.duration}</section>
+                          <section class="article__responsibilities"></p>${currentitem.responsibilities}</p></section>
+                         </article>`
+
+  })
+})
+/* //target the DOM element that will contain the blog articles
 
 //iterate over the key in the retrievedBlogEntries object
 for (var key in retrievedJobEntries) {
@@ -13,11 +26,6 @@ for (var key in retrievedJobEntries) {
     for (var i = 0; i < current.length; i++) {
         //set the currentitem to the value of the current array
         let currentitem = current[i];
-        element.innerHTML += `<article class="article under">
-                              <section class="article__company"><br>${currentitem.company}</section>
-                              <section class="articleHeader">${currentitem.title}</section>
-                              <section class="article__duration">${currentitem.duration}</section>
-                              <section class="article__responsibilities"></p>${currentitem.responsibilities}</p></section>
-                             </article>`
     }
 }
+ */
